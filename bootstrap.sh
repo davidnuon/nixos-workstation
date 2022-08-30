868ce3ad515f1a24f515d04d42e8d9ba94683106
davidnuon <davidnuongm@gmail.com>
2022-08-29 23:05:19 -0700

    Add installing GNU Make to boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Installing home-manager channel..."
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
 nix-channel --update
+
+echo "Installing GNU Make..."
+nix-env --install gnumake
