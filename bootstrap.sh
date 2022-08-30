c2e28aa96e07d5a19a97dc14631d842630181493
davidnuon <davidnuongm@gmail.com>
2022-08-29 23:18:19 -0700

    Updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 echo "Installing home-manager channel..."
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
 nix-channel --update
+
+echo "Making home-manager directory..."
+mkdir -pv "$HOME/.config/nixpkgs"
 
 echo "Installing GNU Make..."
 nix-env --install gnumake
