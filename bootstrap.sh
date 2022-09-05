2f876b17e1b1c99e1d17e8d76df20178fbe1af71
David Nuon <davidnuongm@gmail.com>
2022-09-05 11:00:40 -0700

    Add NIX_PATH to bootstrap to make home-manager work

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 echo "Installing home-manager channel..."
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
 nix-channel --update
+export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
 nix-shell '<home-manager>' -A install
 
 echo "Making home-manager directory..."
